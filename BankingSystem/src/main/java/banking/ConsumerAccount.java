5d3102838b8aa73e0bbfefce962fd46f386c5d62
Arvind Kumar Rai <arvindr389@gmail.com>
2022-09-06 09:51:41 +0530

    Added Banking system project

--- BankingSystem/src/main/java/banking/ConsumerAccount.java
+++ BankingSystem/src/main/java/banking/ConsumerAccount.java
@@ -2,7 +2,6 @@ package banking;
 
 public class ConsumerAccount extends Account {
 	public ConsumerAccount(Person person, Long accountNumber, int pin, double currentBalance) {
-		///Tetstst
 		super(person, accountNumber, pin , currentBalance);
 	}
 }
